0a6e7113c03678bf5c75412a71cbb18acaa88df8
WenbinTeng <WenbinTeng@163.com>
2022-06-17 16:33:47 +0800

    remake

--- sim/include/_74x244.v
+++
@@ -1,9 +0,0 @@
-module _74x244 (
-    input [1:0]g,   // Output Enable (Active LOW)
-    input [7:0] a,  // Data Inputs
-    output [7:0] y  // Data Outputs
-);
-
-    assign y = {g[1] ? 4'bzzzz : a[7:4], g[0] ? 4'bzzzz : a[3:0]};
-
-endmodule
